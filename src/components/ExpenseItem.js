6cb7d2928b5d47b049298ee42b2cec6929749b6f
Dorian Buck <[dorianbuck@gmail.com]>
2021-07-26 17:43:58 +0200

    linked css and fixed typos!

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,14 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+    return (
+    <div className="expense-item">
+      <div>Date</div>
+      <div className="expense-item__description">
+        <h2>Title</h2>
+      </div>
+      <div className="expense-item__price">$294.67</div>
+    </div>
+  );
 }
-export default ExpenseItem
+export default ExpenseItem;
