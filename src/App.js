e58e6928bed8be1ce8f239269f2261c6c0788a29
Dorian Buck <[dorianbuck@gmail.com]>
2021-07-26 22:21:41 +0200

    Added some folders and clean up some code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
 
-function App(props) {
+const App = (props) => {
   const expenses = [
     {
       id: "e1",
