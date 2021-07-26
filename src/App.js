df50eb7bf94107e779652a137d9a27246f3c485e
Dorian Buck <[dorianbuck@gmail.com]>
2021-07-26 19:56:04 +0200

    Added another component to seperate the expenses from app.js. It's now passing instead

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import Expenses from "./components/Expenses";
 import ExpenseItem from "./components/ExpenseItem";
 
-function App() {
+function App(props) {
   const expenses = [
     {
       id: "e1",
@@ -22,30 +23,9 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <h2>Let's get started!</h2> <Expenses expenses={expenses} />
     </div>
   );
 }
