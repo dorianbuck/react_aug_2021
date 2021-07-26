9527ee261617bcda243d3af05124169b8155ed3b
Dorian Buck <[dorianbuck@gmail.com]>
2021-07-26 18:23:03 +0200

    using props methods

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    return (
+function ExpenseItem(props) {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
+  return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
+        <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">$294.67</div>
+      <div className="expense-item__price">${props.amount}</div>
     </div>
   );
 }
